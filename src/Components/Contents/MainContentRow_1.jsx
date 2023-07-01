c43abf02c2a0e85c41c1655ec7671893ad8a9a4b
devil <devil@DevilSensai.netis.cc>
2023-07-01 23:50:13 +0545

    fix minor changes

--- src/Components/Contents/MainContentRow_1.jsx
+++ src/Components/Contents/MainContentRow_1.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./MainContentRow_1.scss";
+import featured_img from "../../Assets/featured_img_1.jpg"
 
 export default function MainContentRow_1() {
   return (
     <div className="main_content_row_1_container">
+      <div className="bg_img">
+        <img src={featured_img} alt="" />
+      </div>
       <div className="main_content_row_1_blur"></div>
       <div className="content">
         <h1>Unlimited movies, TV shows, and more</h1>
